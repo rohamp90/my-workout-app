d2d22f4e9c27c86c42a12d9c0e47d761740c37d9
Roham Pardakhtim <roham.p@gmail.com>
2024-09-19 13:27:43 -0700

    added mongoDB string

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 // Connect to MongoDB (replace <dburl> with your MongoDB connection string)
-mongoose.connect('<dburl>', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('process.env.MONGODB_URI', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Could not connect to MongoDB', err));
 
